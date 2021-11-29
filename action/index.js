a64d9b3a81b0dbb5148d4c6a3b91ed941412bfca
Josh Koike <joshua.koike@accenture.co>
2021-11-29 11:03:06 -0800

    Make action top level function anonymous

--- action/index.js
+++ action/index.js
@@ -5,12 +5,11 @@ const octokit = gh.getOctokit(token)
 const api = octokit.rest;
 
 const context = gh.context.payload;
-async function run() {
+(async () => {
     const issue = await api.issues.get({
         ...context.issue,
         issue_number: context.issue.number
     });
 
     console.log(`Issue #${context.issue.number}: ${JSON.stringify(issue.data)}`)
-};
-run()
+})()
